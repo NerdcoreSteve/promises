543260f6099bd9876e7cb8af24167e8190389982
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2016-12-01 15:10:21 -0700

    added some more callback vs promise code

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -1,3 +1,8 @@
+const
+    R = require('ramda'),
+    $ = require('jquery'),
+    tap = x => { console.log(x); return x }
+
 //Callbacks
 const takeCallback = (f) => f()
 
@@ -5,7 +10,6 @@ takeCallback(() => console.log('potatoes!!'))
 //prints potatoes!!
 
 //The old way to do AJAX
-const $ = require('jquery')
 $.ajax({
   url: '/ajax',
   success: data => console.log(data),
@@ -26,9 +30,57 @@ const $get = path =>
 $get('/ajax').then(data => console.log(data))
 //prints { message: "This comes from an AJAX call!" }
 
+//You might need the babel polyfil for promises,
+//https://babeljs.io/docs/usage/polyfill/
+//but I didn't in Chrome, Firefox, or Safari
+
 //Now This
-//TODO show pyramid of doom with some try catches thrown in
-//
+$.ajax({
+  url: '/heroes',
+  success: results =>
+    $.ajax({
+        url: `/sidekicks/${
+            R.pipe(
+                R.prop('heroes'),
+                R.find(hero => hero.name === 'Batman'),
+                R.prop('id'))
+                    (results)}`,
+        success: results => $.ajax({
+            url: `/firstappearance/${
+                R.pipe(
+                    R.prop('side_kicks'),
+                    R.last,
+                    R.prop('id'))
+                        (results)}`,
+            success: firstappearance => console.log(firstappearance),
+            error: console.error
+        }),
+        error: console.error
+    }),
+  error: console.error
+})
 
 //Turns into this
 //TODO show nice then chain with catch at the end
+$get('/heroes')
+    .then(results =>
+        $get(`/sidekicks/${
+            R.pipe(
+                R.prop('heroes'),
+                R.find(hero => hero.name === 'Batman'),
+                R.prop('id'))
+                    (results)}`))
+    .then(results =>
+        $get(`/firstappearance/${
+            R.pipe(
+                R.prop('side_kicks'),
+                R.last,
+                R.prop('id'))
+                    (results)}`))
+    .then(firstappearance => console.log(firstappearance))
+    .catch(console.error)
+
+//Show Promise .all, .reject, .resolve, .race
+
+//Note that this is side-effect-y as heck and say that Tasks is an alternative to managing
+//network calls
--- index.js
+++ index.js
@@ -13,6 +13,93 @@ app.get('/ajax', function (req, res) {
     })
 })
 
+app.get('/heroes', function (req, res) {  
+    res.json({
+        heroes: [
+            {
+                'id': 1,
+                'name': 'Wonder Woman'
+            },
+            {
+                'id': 2,
+                'name': 'Superman'
+            },
+            {
+                'id': 3,
+                'name': 'Batman'
+            },
+            {
+                'id': 4,
+                'name': 'The Flying Squirrel'
+            },
+            {
+                'id': 5,
+                'name': 'The Red Panda'
+            }
+        ]
+    })
+})
+
+app.get('/sidekicks/:heroid', function (req, res) {  
+    switch(parseInt(req.params.heroid)) {
+        case 3:
+            return res.json({
+                side_kicks: [
+                    {
+                        'id': 6,
+                        'name': 'Robin',
+                        'real name': 'Dick Grayson'
+                    },
+                    {
+                        'id': 7,
+                        'name': 'Robin',
+                        'real name': 'Jason Todd'
+                    },
+                    {
+                        'id': 8,
+                        'name': 'Robin',
+                        'real name': 'Tim Drake'
+                    },
+                    {
+                        'id': 9,
+                        'name': 'Robin',
+                        'real name': 'Damian Wayne'
+                    },
+                    {
+                        'id': 10,
+                        'name': 'Robin',
+                        'real name': 'Stephanie Brown'
+                    },
+                    {
+                        'id': 11,
+                        'name': 'Robin',
+                        'real name': 'Carrie Kelly'
+                    }
+                ]
+            })
+        default:
+            return res.status(400).send({ error: 'Hero id not found' })
+    }
+})
+
+app.get('/firstappearance/:heroid', function (req, res) {  
+    console.log('firstappearance');
+    switch(parseInt(req.params.heroid)) {
+        case 11:
+            return res.json({
+                'id': 11,
+                'name': 'Robin',
+                'real name': 'Carrie Kelly',
+                'first_appearance': {
+                    'title': 'Batman: The Dark Knight Returns',
+                    'year': 1986
+                }
+            })
+        default:
+            return res.status(400).send({ error: 'Hero id not found' })
+    }
+})
+
 app.listen(3000, function () {  
     console.log('Example app listening on port 3000!')
 })
